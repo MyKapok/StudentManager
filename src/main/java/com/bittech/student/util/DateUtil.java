a3990734a10361c572d3a6a534e2d0b9f0323629
MyKapok <43841481+MyKapok@users.noreply.github.com>
2019-04-17 21:29:06 +0800

    Update DateUtil.java

--- src/main/java/com/bittech/student/util/DateUtil.java
+++ src/main/java/com/bittech/student/util/DateUtil.java
@@ -3,8 +3,12 @@ package com.bittech.student.util;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+本类用来进行时间的格式化
+*/
 public class DateUtil {
 
+//传入日期，在传入指定格式
 	public static String formatDate(Date date,String format){
 		String result="";
 		SimpleDateFormat sdf=new SimpleDateFormat(format);
@@ -15,6 +19,7 @@ public class DateUtil {
 	}
 	
 	
+//传入字符串，再传入指定的格式
 	public static Date formatString(String str,String format) throws Exception{
 		SimpleDateFormat sdf=new SimpleDateFormat(format);
 		return sdf.parse(str);
